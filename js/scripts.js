ea57df5e8104034d7db01c6efc0ca9495fe9e478
Martin272 <73481406+Martin272@users.noreply.github.com>
2020-11-11 16:33:06 -0300

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -3,10 +3,15 @@
     * Copyright 2013-2020 Start Bootstrap
     * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-creative/blob/master/LICENSE)
     */
-    (function($) {
+
+      (function($) {
   "use strict"; // Start of use strict
 
+
   // Smooth scrolling using jQuery easing
+
+  
+
   $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
       var target = $(this.hash);
@@ -20,7 +25,7 @@
     }
   });
 
-  // Closes responsive menu when a scroll trigger link is clicked
+    // Closes responsive menu when a scroll trigger link is clicked
   $('.js-scroll-trigger').click(function() {
     $('.navbar-collapse').collapse('hide');
   });
